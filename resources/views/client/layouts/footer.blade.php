558224ceff54ab867827550962982f79e53f3460
SorakpheaNukma <maosorakpheanukma@gmail.com>
2025-04-14 15:56:17 +0700

    TEST

--- resources/views/client/layouts/footer.blade.php
+++ resources/views/client/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h2 class="fw-bold">SkinCare</h2>
+                <h2 class="fw-bold">SAFE 24H</h2>
             </div>
         </div>
 
