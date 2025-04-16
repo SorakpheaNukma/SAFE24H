3fe054db4da7c2506c277749292b3039358efc0a
SorakpheaNukma <maosorakpheanukma@gmail.com>
2025-04-16 13:51:51 +0700

    footerfooter

--- resources/views/client/layouts/footer.blade.php
+++ resources/views/client/layouts/footer.blade.php
@@ -7,36 +7,41 @@
         </div>
 
         <div style="display:flex; flex-direction: row; margin-top: 20px;">
-            <div style="display:flex; flex-direction: column;">
-                <h6 class="sizeFooter">ព័ត៌មានទូទៅ</h6>
-                <ul class="list-unstyled">
-                    <li><a href="#" class="text-white hideTextDecoration">អំពីពួកយើង</a></li>
-                    <li><a href="#" class="text-white hideTextDecoration">ផលិតផលថ្មី</a></li>
-                </ul>
-            </div>
-            <div style="display:flex; flex-direction: column;margin-left:auto;margin-top: 20px;">
-                <h6 class="sizeFooter">ទំនាក់ទំនងពួកយើង</h6>
-                <p>អ៊ីម៉ែល: <a href="https://www.facebook.com/uniforms.expertkh" class="text-white">SAFE24H@gmail.com</a></p>
-                <p>លេខទូរស័ព្ទ: +855 17 725 050</p>
-                <!--<p>Telegram: @SAFE 24H</p> -->
-                <a href="https://t.me/uniforms_expert" style="text-decoration: none;">
-                    <p style="color: white;"> តេលេក្រាម: @SAFE 24H</p>
-                </a>
-                <div class="social-icons">
-                    <a href="https://www.facebook.com/uniforms.expertkh" class="text-white hideTextDecoration fab fa-facebook fa-2x"></a>
-                    <a href="https://t.me/uniforms_expert" class="text-white hideTextDecoration mx-2 fab fa-telegram fa-2x"></a>
-                    <!--<a href="Uniforms Expert KH" class="text-white hideTextDecoration mx-2 fab fa-tiktok fa-2x"></a> -->
+         <!-- Phần trái -->
+        <div style="display:flex; flex-direction: column;">
+            <h6 class="sizeFooter">ព័ត៌មានទូទៅ</h6>
+            <ul class="list-unstyled">
+                <li><a href="#" class="text-white hideTextDecoration">អំពីពួកយើង</a></li>
+                <li><a href="#" class="text-white hideTextDecoration">ផលិតផលថ្មី</a></li>
+            </ul>
+        </div>
 
-                </div>
-            </div>
-            <div style="padding:10px; margin-left: 20px;">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3710.9450438650415!2d105.84230927532627!3d21.549004080233775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313521406ca0840d%3A0x40cb6f8d2cad2168!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBL4bu5IFRodeG6rXQgQ8O0bmcgTmdoaeG7h3A!5e0!3m2!1svi!2s!4v1744781848841!5m2!1svi!2s" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <!-- Phần liên hệ -->
+        <div style="display:flex; flex-direction: column; align-items: center; margin-left: 20px;">
+            <h6 class="sizeFooter">ទំនាក់ទំនងពួកយើង</h6>
+            <p>អ៊ីម៉ែល: <a href="https://www.facebook.com/uniforms.expertkh" class="text-white">SAFE24H@gmail.com</a></p>
+            <p>លេខទូរស័ព្ទ: +855 17 725 050</p>
+            <a href="https://t.me/uniforms_expert" style="text-decoration: none;">
+                <p style="color: white;"> តេលេក្រាម: @SAFE 24H</p>
+            </a>
+            <div class="social-icons">
+                <a href="https://www.facebook.com/uniforms.expertkh" class="text-white hideTextDecoration fab fa-facebook fa-2x"></a>
+                <a href="https://t.me/uniforms_expert" class="text-white hideTextDecoration mx-2 fab fa-telegram fa-2x"></a>
             </div>
         </div>
 
+        <!-- Phần bản đồ Google Maps -->
+        <div style="padding:5px; margin-left: 20px;">
+                <iframe src="https://www.google.ca/maps/place/Uniforms+Expert/@11.5588933,104.878445,17z/data=!3m1!4b1!4m6!3m5!1s0x310951707c6f4103:0x73a2fdcf91f9f19d!8m2!3d11.5588881!4d104.8810199!16s%2Fg%2F11qbsbb9ry?entry=ttu&g_ep=EgoyMDI1MDQxMy4wIKXMDSoASAFQAw%3D%3D"
+                    width="500" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+            </div>
+        </div>
+
+
         
     </div>
-    <div style="display:flex; align-items: center;justify-content: center;">
+    <div style="display:flex; align-items: center;justify-content: left;">
             <p style="margin:0;">© 2024, All rights reserved</p>
         </div>
 </footer>
